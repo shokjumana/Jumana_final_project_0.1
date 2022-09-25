5a48b61b0bb323e07ea0da8cfc89b30fa8f96580
Owner <alsalamcse>
2022-09-25 14:04:09 +0300

    sign in

---
+++ app/src/main/java/shk/jumana/jumanasfinalproject/Sign_In.java
@@ -0,0 +1,18 @@
+package shk.jumana.jumanasfinalproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Sign_In extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+
+
+
+
+    }
+}
--- app/src/main/java/shk/jumana/jumanasfinalproject/Splash_Screen.java
+++ app/src/main/java/shk/jumana/jumanasfinalproject/Splash_Screen.java
@@ -2,13 +2,34 @@ package shk.jumana.jumanasfinalproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class Splash_Screen extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash_screen);
+        setContentView(R.layout.activity_splash_screen2);
+
+
+
+
+        Handler H = new Handler();
+        Runnable r= new Runnable() {
+            @Override
+            public void run() {
+                // to open new activity and move from current page to the next one
+                Intent i= new Intent(Splash_Screen.this,Sign_In.class);
+                startActivity(i);
+                //to close current page
+                finish();
+
+            }
+        };
+
+        H.postDelayed(r,3000);
+
     }
 }
