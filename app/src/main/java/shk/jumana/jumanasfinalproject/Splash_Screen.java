4ee765180af4cf3f2bddc52cc7f8c272f22d7081
Owner <alsalamcse>
2022-09-26 14:00:48 +0300

    splash screen

--- app/src/main/java/shk/jumana/jumanasfinalproject/Splash_Screen.java
+++ app/src/main/java/shk/jumana/jumanasfinalproject/Splash_Screen.java
@@ -11,7 +11,7 @@ public class Splash_Screen extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash_screen2);
+        setContentView(R.layout.activity_splash_screen);
 
 
 
