0e853be09b2fcb632fd9fa5df3ef79739417fcc2
Owner <alsalamcse>
2022-09-29 11:38:38 +0300

    sign up page not finished

--- app/src/main/java/shk/jumana/jumanasfinalproject/Sign_In.java
+++ app/src/main/java/shk/jumana/jumanasfinalproject/Sign_In.java
@@ -1,18 +1,127 @@
 package shk.jumana.jumanasfinalproject;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class Sign_In extends AppCompatActivity {
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class Sign_In extends AppCompatActivity
+{
+
+    private TextInputEditText etEmail1;
+    private TextInputEditText etPassword1;
+    private Button btnSignIn1;
+    private Button btnSignUp1;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
 
+        etEmail1=findViewById(R.id.etEmail1);
+        etPassword1=findViewById(R.id.etPassword1);
+        btnSignIn1=findViewById(R.id.btnSignIn1);
+        btnSignUp1=findViewById(R.id.btnSignUp2);
 
 
+
+        btnSignUp1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+
+                Intent i = new Intent (Sign_In.this,Sign_Up.class);
+                startActivity(i);
+
+            }
+        });
+
+
+        btnSignIn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+
+                CheckAndSave();
+
+            }
+        });
+
+    }
+
+    private void CheckAndSave()
+    {
+
+        String Email=etEmail1.getText().toString();
+        String Password=etPassword1.getText().toString();
+        boolean isOk=true;
+
+
+
+
+        if (Email.length()==0)
+        {
+            etEmail1.setError("please enter your email");
+            isOk=false;
+            //checks if email=0
+        }
+
+        if (Password.length()==0)
+        {
+            etPassword1.setError("please enter your Password");
+            isOk=false;
+            //checks if password=0
+        }
+
+        if (Email.indexOf("@")<=0)
+        {
+            etEmail1.setError("your email is wrong");
+            isOk=false;
+            //checks if email hqs @
+        }
+
+        if (Password.length()<7)
+        {
+            etPassword1.setError("your password should be at least 7 characters");
+            isOk=false;
+            //checks if password has less then 7 characters =0
+        }
+
+
+        if (isOk)
+        {
+            FirebaseAuth auth=FirebaseAuth.getInstance();
+            auth.signInWithEmailAndPassword(Email,Password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task)
+                {
+                    if (task.isSuccessful())
+                    {
+                        Toast.makeText(Sign_In.this, "Successful", Toast.LENGTH_SHORT).show();
+                        Intent i = new Intent(Sign_In.this,MainActivity.class);
+                        startActivity(i);
+                        finish();
+                    }
+
+                    else
+                        Toast.makeText(Sign_In.this, "Not Successful"+task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+
+                }
+            });
+
+
+        }
 
     }
 }
--- app/src/main/java/shk/jumana/jumanasfinalproject/Sign_Up.java
+++ app/src/main/java/shk/jumana/jumanasfinalproject/Sign_Up.java
@@ -1,14 +1,92 @@
 package shk.jumana.jumanasfinalproject;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class Sign_Up extends AppCompatActivity {
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class Sign_Up extends AppCompatActivity
+{
+    private TextInputEditText etEmail2;
+    private TextInputEditText etPassword2;
+    private TextInputEditText etConfirmPassword;
+    private Button btnSave;
+
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+
+
+
+        etEmail2=findViewById(R.id.etEmail2);
+        etPassword2=findViewById(R.id.etPassword2);
+        etConfirmPassword=findViewById(R.id.etConfirmPassword);
+        btnSave=findViewById(R.id.btnSave);
+
+
+        btnSave.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                CheckAndSave();
+
+            }
+
+            private void CheckAndSave() {
+                String Email = etEmail2.getText().toString();
+                String Password = etPassword2.getText().toString();
+                String ConfirmPassword = etEmail2.getText().toString();
+
+                boolean isOk = true;
+
+                if (Email.length() * Password.length() * ConfirmPassword.length() == 0)//تفحص اذا كلمة السر وتأكيد كلمة السر يساوون 0
+                {
+                    etEmail2.setError("one of the files are empty");
+                    isOk = false;
+                }
+
+                if (Password.equals(ConfirmPassword) == false)//تفحص اذا كلمة السر تساوي تاكيد كلمة السر
+                {
+                    etConfirmPassword.setError("your password does not match");
+                    isOk = false;
+                }
+
+                if (isOk) {
+                    FirebaseAuth auth = FirebaseAuth.getInstance();
+                    auth.createUserWithEmailAndPassword(Email, Password).addOnCompleteListener(new OnCompleteListener<AuthResult>()
+                    {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful())
+                            {
+                                Toast.makeText(Sign_Up.this, "creation successful", Toast.LENGTH_SHORT).show();
+                                finish();
+
+                            }
+                            else {
+                                Toast.makeText(Sign_Up.this, "creation failed" + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+
+                            }
+
+
+                        }
+                    });
+                }
+            }
+        }
     }
 }
