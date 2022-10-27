314b1b2c213b382b1fd0b1d4ac308715fb1eaa6a
shokjumana <jumanashok55@gmail.com>
2022-10-27 11:39:08 +0300

    did the book and its adj.

--- app/src/main/java/shk/jumana/jumanasfinalproject/data/Book.java
+++ app/src/main/java/shk/jumana/jumanasfinalproject/data/Book.java
@@ -2,18 +2,23 @@ package shk.jumana.jumanasfinalproject.data;
 
 public class Book
 {
-
-
     private String key;
-    private String title;
-    private String subject;
-    private String owners;
-
+    private String owner;
+    private String Title;
+    private String name;
+    private String Author;
+    private int year;
+    private int age;
+    private String Genre;
+    private int pages;
+    private int rate;
+    private String About;
 
     public Book()
     {
 
     }
+
     public String getKey() {
         return key;
     }
@@ -22,47 +27,101 @@ public class Book
         this.key = key;
     }
 
+    public String getOwner() {
+        return owner;
+    }
+
+    public void setOwner(String owner) {
+        this.owner = owner;
+    }
+
     public String getTitle() {
-        return title;
+        return Title;
     }
 
     public void setTitle(String title) {
-        this.title = title;
+        Title = title;
     }
 
-    public String getSubject() {
-        return subject;
+    public String getName() {
+        return name;
     }
 
-    public void setSubject(String subject) {
-        this.subject = subject;
+    public void setName(String name) {
+        this.name = name;
     }
 
-    public String getOwners() {
-        return owners;
+    public String getAuthor() {
+        return Author;
     }
 
-    public void setOwners(String owners) {
-        this.owners = owners;
+    public void setAuthor(String author) {
+        Author = author;
     }
 
+    public int getYear() {
+        return year;
+    }
 
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getGenre() {
+        return Genre;
+    }
+
+    public void setGenre(String genre) {
+        Genre = genre;
+    }
+
+    public int getPages() {
+        return pages;
+    }
+
+    public void setPages(int pages) {
+        this.pages = pages;
+    }
+    public int getRate() {
+        return rate;
+    }
+
+    public void setRate(int rate) {
+        this.rate = rate;
+    }
+
+    public String getAbout() {
+        return About;
+    }
+
+    public void setAbout(String about) {
+        About = about;
+    }
 
 
     @Override
     public String toString() {
         return "Book{" +
                 "key='" + key + '\'' +
-                ", title='" + title + '\'' +
-                ", subject='" + subject + '\'' +
-                ", owners='" + owners + '\'' +
+                ", owner='" + owner + '\'' +
+                "Title ="+ Title+'\'' +
+                ", name='" + name + '\'' +
+                ", Author='" + Author + '\'' +
+                ", year=" + year +
+                ", age=" + age +
+                ", Genre='" + Genre + '\'' +
+                ", pages=" + pages +
+                ", Rate = " +rate+
+                ", About = "+ About+
                 '}';
     }
-
-
-
-
-
-
 
 }
